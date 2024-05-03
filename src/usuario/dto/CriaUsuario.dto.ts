373c8fa24536879b80a6455e5b2885ee692b40b7
NiltonDionisioGuerra <nguerra@ogmaster.local>
2024-05-03 12:36:37 -0300

    teste

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -7,7 +7,7 @@ export class CriaUsuarioDTO{
     @IsNotEmpty({message: "por favor preencha o campo nome"})
     nome:string;
 
-    @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
+    @IsEmail(undefined, { message: 'O e-mail informado é inválido!' })
     @EmailEhUnico({ message: 'Já existe um usuário com este e-mail' })
     email: string;
 
